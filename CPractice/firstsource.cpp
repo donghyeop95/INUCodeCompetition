e49698390c8c36627777c80076daba97b90b4536
donghyeop95 <donghyeop95@gmail.com>
2021-04-26 16:52:54 +0900

    array size 입력받기

--- CPractice/firstsource.cpp
+++ CPractice/firstsource.cpp
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
 	int N, X, K, Ai, Bi;
-	char arr[N];
+	int* arr;
 	scanf_s("%d %d %d", &N, &X, &K);
 
-	for (int i = 0; i < K; i++) {
+	/*for (int i = 0; i < K; i++) {
 		scanf_s("%d %d", &Ai, &Bi);
+	}*/
+	arr = (int *)malloc(sizeof(int) * X);
+	for (int i = 0; i < X; i++)
+	{
+		scanf_s("%d", &arr[i]);
 	}
-	for (int i = 0; i < N; i++) {
-		arr[i] = 0;
+	for (int i = 0; i < X; i++)
+	{
+		printf("%d ", arr[i]);
 	}
-	for (int i = 0; i < N; i++) {
-		printf("%d", arr[i]);
-	}
+
+	free(arr);
+	return 0;
 }
